d2132e4242aa46d44a09ea28c989052f69a577f4
ValeraBladee <potniyschenok@gmail.com>
2024-02-02 13:25:15 +0200

    fix img

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const jimp = require("jimp");
 
 const { User } = require("../models/user");
 
@@ -78,10 +79,17 @@ const logout = async (req, res) => {
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
+
   const { path: tempUpload, originalname } = req.file;
+
+  const image = await jimp.read(tempUpload);
+  await image.resize(250, 250).writeAsync(tempUpload);
+
   const filename = `${_id}_${originalname}`;
+
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
+
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
